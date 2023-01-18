e460ff8bbbd22de5786a15fec7f233219020a1d3
Caleb Short <shortcaleb@gmail.com>
2023-01-18 15:12:21 -0500

    first day work

--- tank-mates/App.js
+++ tank-mates/App.js
@@ -1,7 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+
+
 export default function App() {
+
+  const feedData = [
+    {id: 1, text: 'this is the post number 1', videoLink: '', webLink: '', user: 'Caleb Short', avatar: ''},
+  ];
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,7 +21,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
