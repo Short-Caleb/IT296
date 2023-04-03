0d89300b3c40471f52654ea85931c737da169098
Caleb Short <shortcaleb@gmail.com>
2023-04-03 15:21:30 -0400

    three pressables working  3 april

--- tank-mates/screens/NewPost.js
+++ tank-mates/screens/NewPost.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect, useRef } from 'react'
-import { SafeAreaView, View, Text, StyleSheet, TextInput, Button } from 'react-native'
+import { SafeAreaView, View, Text, StyleSheet, TextInput, Button, Alert, Modal, Pressable, ImagePickerIOS, Image } from 'react-native'
+import * as ImagePicker from 'expo-image-picker';
 import { FontAwesome5 } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
 import { Entypo } from '@expo/vector-icons';
+import {Video,  AVPlaybackStatus } from 'expo-av'
+
 
 export const NewPost = () => {
   
@@ -11,15 +14,45 @@ export const NewPost = () => {
   const [videoLink, setVideoLink] = useState('');
   const [ picLink, setPicLink] = useState('');
   const [ webLink, setWebLink] = useState('');
-
+  const [modalvisible, setModalVisible] = useState(false)  
+  const [image, setImage] = useState(null);
+  const [video, setVideo] = useState(null)
   const inputRef = useRef(null);
 
+const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: false,
+        quality: 1,
+        });
+        console.log(result);
+
+        if (!result.canceled) {
+            setImage(result.assets[0].uri)
+        }
+}
+
+const pickVideo = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Videos,
+        allowsEditing: false,
+        quality: 1,
+        });
+        console.log(result);
+
+        if (!result.canceled) {
+            setVideo(result.assets[0].uri)
+        }
+}
 
 
   useEffect( () => {
     inputRef.current.focus();
    }, []);
   
+
+
+
     return (
     <SafeAreaView style={styles.container}>
      
@@ -27,15 +60,65 @@ export const NewPost = () => {
      <View style={styles.post}>
         <Text style={styles.title}>NEW POST</Text>
        <Text style={styles.text}>{postText}</Text>
+       <Text style={styles.linktext}>{webLink}</Text>
+       <Text>{image && <Image source={{uri: image}} style={styles.image} />}</Text>
+       <Text>{video && <Video source={{uri: video}} style={styles.image} />} </Text>
      </View>
+     <Modal
+        animationType='slide'
+        transparent={true}
+        visible={modalvisible}
+        onRequestClose={() => {
+            Alert.alert('Modal is closing now!');
+            setModalVisible(!modalvisible);
+        }}>
+
+    <View style={styles.centeredView}>
+        <View style={styles.modalView}>
+        <Text style={styles.modalText}>does this work </Text>
+       <View style={styles.modalInput}>
+        <TextInput
+        style={styles.textbox}
+        editable
+        multiline
+        maxLength={240}
+        numberOfLines={2}
+        onChangeText={setWebLink}
+        value={webLink}
+        />
+        </View>
+        <Pressable 
+        style={[styles.button, styles.buttonClose]}
+        onPress={() => {
+            setModalVisible(!modalvisible)
+        }}
+        >
+            <Text style={styles.text}>Add webLink!</Text>
+        </Pressable>
+        </View>
+    </View>
+   </Modal>
+      
+
      <View style={styles.iconRow} >
         <View style={styles.icons}>
-        <FontAwesome5 name="link" size={24} color="white" />
+        <Pressable
+        onPress={() => setModalVisible(true)}>  
+        <FontAwesome5 name="link" size={24} color="white" />          
+        </Pressable>
+
+        <Pressable
+        onPress={pickImage}>
         <AntDesign name="picture" size={24} color="white" />
+        </Pressable>
+        <Pressable 
+        onPress={pickVideo}>
         <Entypo name="video-camera" size={24} color="white" />
+        </Pressable>
         </View>
         <Button title='Submit'></Button>
      </View>
+     
      <View style={styles.input}>
         <TextInput
         style={styles.textbox}
@@ -47,7 +130,7 @@ export const NewPost = () => {
             value={postText}
         />
      </View> 
-   
+ 
     </SafeAreaView>
   )
 }
@@ -112,4 +195,58 @@ const styles = StyleSheet.create({
         fontSize: 28,
         fontWeight: 'bold',
     },
+    button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+        
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
+      },
+      modalView: {
+        margin: 10,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 15,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+      },
+      buttonOpen: {
+        backgroundColor: 'blue',
+      },
+      buttonClose: {
+        backgroundColor: 'blue',
+      },
+      modalText: { 
+        color: 'black',
+        fontSize: 14,
+    },
+    linktext: {
+        color: 'lightblue',
+        fontSize: 14,
+        textDecorationLine: 'underline'
+    },
+    modalInput: { 
+        borderColor: 'black',
+        borderWidth: 2,
+        borderRadius: 2,
+        color: 'black',
+        width: '100%'
+    },
+    image: {
+        width: 200,
+        height: 200,
+      },
+
 })
